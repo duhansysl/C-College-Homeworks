78405f066b3624c63478134f4dd106d2f8e8eab5
duhań <duhan2002@gmail.com>
2023-05-07 18:35:07 +0300

    HW2-1 | Update for visual beautyness

--- HW2/HW2-220223055_1.c
+++ HW2/HW2-220223055_1.c
@@ -10,24 +10,30 @@ void main()
 double equation_one[3], equation_two[3], intersection[2]; 
 	
 	// Burada ilk dizimin elemanlarinin girisini kullanicidan istedim.
-	
+
+	printf("---------------------------------------\n"); 	
 	printf("Ilk dogrunun parametrelerini giriniz:\n");	   
-  
-	printf("a1:"); 
+	printf("---------------------------------------\n\n"); 
+		
+	printf("------\n a1:"); 
  		scanf("%lf", &equation_one[0]);    
- 	printf("b1:");
+ 	printf("------\n b1:");
  		scanf("%lf", &equation_one[1]);
- 	printf("c1:");
+ 	printf("------\n c1:");
    		scanf("%lf", &equation_one[2]);
+ 	printf("------\n\n");	    		
    		
    	// Burada ise ikinci dizimin elemanlarinin girisini kullanicidan istedim.
-   
+
+	printf("---------------------------------------\n"); 
    	printf("Ikinci dogrunun parametrelerini giriniz:\n");
+	printf("---------------------------------------\n\n"); 
    	
-   	printf("a2:");
+	   printf("------\n a1:");
   		scanf("%lf", &equation_two[0]);
-	printf("b2:");
+	printf("------\n b1:");
   		scanf("%lf", &equation_two[1]);  
-  	printf("c2:");
-   		scanf("%lf", &equation_two[2]);		
+  	printf("------\n c1:");
+   		scanf("%lf", &equation_two[2]);
+	printf("------\n");	  	   	
 }
