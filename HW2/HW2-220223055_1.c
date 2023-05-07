c773bac331b4843bdb4b64642c6847f7ce0be479
duhań <duhan2002@gmail.com>
2023-05-07 17:58:38 +0300

    HW2-1 | Add scanf for equations to add into series
    * will take values as long-float & put it to the series
    * defined series as "double" to avoid problems
    * added expressions

--- HW2/HW2-220223055_1.c
+++ HW2/HW2-220223055_1.c
@@ -2,4 +2,32 @@
 
 #include <stdio.h>
 
-
+void main()
+{
+double equation-one[3], equation-two[3], intersection[2];  // Burada dizimin tanimlamasini "double" veri tipiyle yaptim.
+	
+	// Burada ilk dizimin elemanlarinin girisini kullanicidan istedim.
+	
+	printf("Ilk dogrunun parametrelerini giriniz:\n");	   
+  
+	printf("a1:"); 
+ 		scanf("%lf", &equation-one[0]);    
+ 	printf("b1:");
+ 		scanf("%lf", &equation-one[1]);
+ 	printf("c1:");
+   		scanf("%lf", &equation-one[2]);
+   		
+   	// Burada ise ikinci dizimin elemanlarinin girisini kullanicidan istedim.
+   
+   	printf("Ikinci dogrunun parametrelerini giriniz:\n");
+   	
+   	printf("a2:");
+  		scanf("%lf", &equation-two[0]);
+	printf("b2:");
+  		scanf("%lf", &equation-two[1]);  
+  	printf("c2:");
+   		scanf("%lf", &equation-two[2]);
+   
+   
+   
+  
