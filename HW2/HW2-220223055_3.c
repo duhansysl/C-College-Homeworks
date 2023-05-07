dc26c779fcf927751d75039d85d617299a41af6d
duhań <duhan2002@gmail.com>
2023-05-07 23:07:47 +0300

    HW2-3 | Import "for" calls
    * Basamak farkını hesaplayıp o farka göre boşluk bıraktırıp alt alta yazdırdım
    * Basamakları kontrol edip hangisinin fazla olduğunu tespit eden ve ettikten sonra da o kadar çizgi çeken bir fonksiyon yazdırdım

--- HW2/HW2-220223055_3.c
+++ HW2/HW2-220223055_3.c
@@ -1,6 +1,7 @@
 // Duhan SOYSAL - 220223055_3
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -8,7 +9,7 @@ int main()
 // Burada girilen sayi sayisinin tamsayi veri tipi ile diziye (N) aktarilmasini sagladim.
 	
 	char sayi1[100000], sayi2[100000];
-	double length1, length2;
+	int basamak_fark, length1, length2, i;
 	printf("---------------------------------------\n");
 	printf("Birinci sayi:");
 	scanf("%s%n", &sayi1, &length1);
@@ -17,8 +18,49 @@ int main()
 	scanf("%s%n", &sayi2, &length2);
 	printf("---------------------------------------\n");
 	printf("\n");
-	printf("%s\n", sayi1);
-	printf("%s\n", sayi2);
-		
+	
+// Burada basamak farkini hesaplayip sayilari duzgun bicimde alt alta yazdirdim.	
+	
+	basamak_fark = abs(length1-length2);
+
+		if (length1 < length2)	
+	{
+		for (i=0;i<basamak_fark;i++)
+		{
+			printf(" ");
+		}
+		printf("%s\n", sayi1);
+		printf(" %s\n", sayi2);	
+	}
+
+	if (length2 < length1)	
+	{
+		printf("%s\n", sayi1);
+		for (i=0;i<basamak_fark;i++)
+		{
+			printf(" ");
+		}
+		printf(" %s\n", sayi2);	
+	}
+	
+// Burada birinci ya da ikinci sayilardan hangisinin basamak sayisi fazlaysa onu bulup o kadar cizgi ceken bir fonksiyon yazdim.	
+	
+	printf("+\n");
+	
+	if (length1 < length2)
+	{
+		for (i=0;i<length2;i++)
+		{
+			printf("-");
+		}	
+	}
+	
+		if (length2 < length1)
+	{
+		for (i=0;i<length1;i++)
+		{
+			printf("-");
+		}	
+	}
 }
 
