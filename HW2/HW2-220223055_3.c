b04b88961b0db09ca5dd913991c898471c495f68
duhań <duhan2002@gmail.com>
2023-05-07 22:40:08 +0300

    HW2-3 | Add scanf for the numbers & printf & series

--- HW2/HW2-220223055_3.c
+++ HW2/HW2-220223055_3.c
@@ -1,3 +1,24 @@
 // Duhan SOYSAL - 220223055_3
 
 #include <stdio.h>
+
+int main()
+{
+	
+// Burada girilen sayi sayisinin tamsayi veri tipi ile diziye (N) aktarilmasini sagladim.
+	
+	char sayi1[100000], sayi2[100000];
+	double length1, length2;
+	printf("---------------------------------------\n");
+	printf("Birinci sayi:");
+	scanf("%s%n", &sayi1, &length1);
+	printf("---------------------------------------\n");
+	printf("Ikinci sayi:");
+	scanf("%s%n", &sayi2, &length2);
+	printf("---------------------------------------\n");
+	printf("\n");
+	printf("%s\n", sayi1);
+	printf("%s\n", sayi2);
+		
+}
+
