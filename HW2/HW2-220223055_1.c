4713cf5f9cd3e2af2fe6f8ce2df2b40a8d84dfae
duhań <duhan2002@gmail.com>
2023-05-07 18:05:51 +0300

    HW2-1 | Fix compilation errors

--- HW2/HW2-220223055_1.c
+++ HW2/HW2-220223055_1.c
@@ -7,30 +7,27 @@ void main()
 
 	// Burada dizimin tanimlamasini "double" veri tipiyle yaptim.
 
-double equation-one[3], equation-two[3], intersection[2]; 
+double equation_one[3], equation_two[3], intersection[2]; 
 	
 	// Burada ilk dizimin elemanlarinin girisini kullanicidan istedim.
 	
 	printf("Ilk dogrunun parametrelerini giriniz:\n");	   
   
 	printf("a1:"); 
- 		scanf("%lf", &equation-one[0]);    
+ 		scanf("%lf", &equation_one[0]);    
  	printf("b1:");
- 		scanf("%lf", &equation-one[1]);
+ 		scanf("%lf", &equation_one[1]);
  	printf("c1:");
-   		scanf("%lf", &equation-one[2]);
+   		scanf("%lf", &equation_one[2]);
    		
    	// Burada ise ikinci dizimin elemanlarinin girisini kullanicidan istedim.
    
    	printf("Ikinci dogrunun parametrelerini giriniz:\n");
    	
    	printf("a2:");
-  		scanf("%lf", &equation-two[0]);
+  		scanf("%lf", &equation_two[0]);
 	printf("b2:");
-  		scanf("%lf", &equation-two[1]);  
+  		scanf("%lf", &equation_two[1]);  
   	printf("c2:");
-   		scanf("%lf", &equation-two[2]);
-   
-   
-   
-  
+   		scanf("%lf", &equation_two[2]);		
+}
