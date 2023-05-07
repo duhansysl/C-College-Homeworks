0264e76e57c68c9180f881d3368cdb7faec864d0
duhań <duhan2002@gmail.com>
2023-05-07 20:32:51 +0300

    HW2-2 | Add "for" for the numbers thats declared above
    * entered values will be added into the serie we defined above
    * added expressions to be more clear

--- HW2/HW2-220223055_2.c
+++ HW2/HW2-220223055_2.c
@@ -3,9 +3,22 @@
 #include <stdio.h>
 
 int main()
-{
-	int i, n;
+{	
+
+// Burada girilen sayi sayisinin tamsayi veri tipi ile diziye (N) aktarilmasini sagladim.
+	
+	int i, N;
 	printf("Eleman sayisini giriniz: ");
-	scanf("%d",&n);
-	double x[n];
+	scanf("%d", &N);
+
+// Burada ustte girilen sayi kadar deger sorulmasini ve girilen degerlerin scanf fonksiyonu ile double veri tipi ile tarandiktan sonra diziye (i) eklenmesini sagladim.
+	
+	double x[N];	
+	for (i=1;i<=N;i++)
+	{
+		printf("%d. sayi: ", i);
+		scanf("%lf", &x[i]);
+	}
 }
+
+
