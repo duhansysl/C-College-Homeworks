ea1d67f6308af1987a875449bcb9f1e277e3b57c
duhań <duhan2002@gmail.com>
2023-05-08 22:20:58 +0300

    HW2-2 | Add expression

--- HW2/HW2-220223055_1.c
+++ HW2/HW2-220223055_1.c
@@ -59,7 +59,7 @@ double x1, y1, s1, x2, y2, s2, x, y;
  intersection_point[0]=(x1*s2-x2*s1)/(x2*y1-x1*y2);
  intersection_point[1]=(y1*s2-y2*s1)/(y2*x1-y1*x2);
  
- //
+ // Burada dogruyu yazdirdim.
  
 	printf("%.2lfx + %.2lfy + %.2lf = 0\n", x1 ,y1, s1);
 	printf("%.2lfx + %.2lfy + %.2lf = 0\n\n", x2 ,y2, s2);
