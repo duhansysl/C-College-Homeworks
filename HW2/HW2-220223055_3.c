3ce47958db5550a56a88c23f3d0871481e16acf0
duhań <duhan2002@gmail.com>
2023-05-08 15:07:15 +0300

    HW2-3 | Add sum calculation

--- HW2/HW2-220223055_3.c
+++ HW2/HW2-220223055_3.c
@@ -8,7 +8,7 @@ int main()
 	
 // Burada girilen sayilarin char veri tipi ile diziye (sayi1&2) aktarilmasini sagladim.
 
-	int basamak_fark, length1, length2, i;
+	int basamak_fark, length1, length2, i, j, a, b, sum;
 	char sayi1[100000], sayi2[100000], toplam[1000000];
 	printf("---------------------------------------\n");
 	printf("Birinci sayi:");
@@ -72,13 +72,81 @@ int main()
 		}	
 	}
 
+
+///////////////////////////////////////////////////////////
+
+	printf("\n     ");	
+	j=length1;
 	
+while(length1>0)
+{
 	
-	printf("\n    ");
+	a=char_to_int(sayi2[length1-basamak_fark]);
+	b=char_to_int(sayi1[length1]);
+    sum=(int)a+b;
+    
+	if (sum>=10)
+	
+	{
+	toplam[length1]=(sum%10);
+	sayi2[length1-basamak_fark-1]+=1;
+    }
+    
+    else 
+    {
+    	toplam[length1]=sum;
+    }
+     
+	length1--;
+ 	
 }
 
+ 	for(j;j>=0;j--)
+ {
+ 	printf("%d",toplam[j]);
+ }
 
+}
 
-
+int char_to_int (int ascii)
+{
+	int sayi;
 	
-
+	switch(ascii)
+	{
+	   case 48:
+	   sayi=0;
+	   break;
+	   case 49:
+	   sayi=1;
+	   break;
+	   case 50:
+	   sayi=2;
+	   break;
+	   case 51:
+	   sayi=3;
+	   break;
+	   case 52:
+	   sayi=4;
+	   break;
+	   case 53:
+	   sayi=5;
+	   break;
+	   case 54:
+	   sayi=6;
+	   break;
+	   case 55:
+	   sayi=7;
+	   break;
+	   case 56:
+	   sayi=8;
+	   break;
+	   case 57:
+	   sayi=9;
+	   break;
+}
+	   
+	   return sayi;
+	
+	
+}
