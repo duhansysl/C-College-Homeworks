61518152ef700f959b09260b72b06f493dae3db5
duhań <duhan2002@gmail.com>
2023-05-18 23:12:45 +0300

    HW3-1 | Import string.h library & explanation

--- HW3/HW3-220223055_1.c
+++ HW3/HW3-220223055_1.c
@@ -1,3 +1,6 @@
 // Duhan SOYSAL - 220223055_1
+// --------------------------------------
+// Girilen cevaplar ile cevap anahtarini karsilastiran program. (GitHub icin aciklama yapiyorum.) 
 
-#include <stdio.h>
+#include  <stdio.h>
+#include  <string.h>
