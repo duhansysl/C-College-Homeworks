615bd42f2456c2e4708e3241dc8b06d1905cc460
duhań <duhan2002@gmail.com>
2023-05-18 23:13:55 +0300

    HW3-1 | Add gets function to fill array & declare needed stuff

--- HW3/HW3-220223055_1.c
+++ HW3/HW3-220223055_1.c
@@ -4,3 +4,19 @@
 
 #include  <stdio.h>
 #include  <string.h>
+
+int main()
+{
+	
+// Burada 100 adetlik bir dizi olusturdum, tanimlamalarimi yaptim ve kullanicidan veri isteyip gets komutu ile dizilere atama yaptim.
+
+    char answers[100], user_input[100];
+    int soru_sayi, length = 0, i = 0, tr = 0, emp = 0;
+    printf("------------------------------------------------------------------\n");
+    printf("Cevap Anahtarini Giriniz        : ");
+    gets(answers);
+    printf("------------------------------------------------------------------\n");    
+    printf("Ogrencinin Cevaplarini Giriniz  : ");
+	gets(user_input);
+	printf("------------------------------------------------------------------");
+	soru_sayi=strlen(answers);
