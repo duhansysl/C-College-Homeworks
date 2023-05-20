90ed93707777879b9c4ac58a9290d43d13c684bc
duhań <duhan2002@gmail.com>
2023-05-20 18:19:31 +0300

    HW3-2 | Import structure
    * added stdio, math. will be using them
    * added function info

---
+++ HW3/HW3-220223055_2.c
@@ -0,0 +1,6 @@
+// Duhan SOYSAL - 220223055_2
+// --------------------------------------
+// Girilen katsayi degerine gore polinom yazan ve girilen x degerine gore kullaniciya polinomun sonucunu veren program. (GitHub icin aciklama yapiyorum.) 
+
+#include <stdio.h>
+#include <math.h>
