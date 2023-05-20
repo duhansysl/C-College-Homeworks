9596dd00b4ad374011b1991ebcbed2dd119e0a91
duhań <duhan2002@gmail.com>
2023-05-20 18:21:22 +0300

    HW3-2 | Import main function

--- HW3/HW3-220223055_2.c
+++ HW3/HW3-220223055_2.c
@@ -7,5 +7,25 @@
 
 int main() 
 {
+    int derece, i;
+    float x;
 
+    printf("Polinomun derecesini girin: ");
+    scanf("%d", &derece);
+
+    float katsayilar[derece + 1];
+
+    for (i = 0; i <= derece; i++) {
+        printf("%d katsayisini girin: ", i+1);
+        scanf("%f", &katsayilar[i]);
+    }
+
+    printf("x degerini girin: ");
+    scanf("%f", &x);
+
+    float sonuc = PolinomHesapla(katsayilar, derece, x);
+
+    printf("Sonuc: %f\n", sonuc);
+
+    return 0;
 }
