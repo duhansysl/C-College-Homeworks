58edb02e2ec318765f73b881abb592b7993dc222
duhań <duhan2002@gmail.com>
2023-05-20 19:22:18 +0300

    HW3-3 | Import structure

---
+++ HW3/HW3-220223055_3.c
@@ -0,0 +1,6 @@
+// Duhan SOYSAL - 220223055_3
+// --------------------------------------
+// Girilen nokta sayilarinca aralarindaki birbirine en uzak olan 2 noktayi bulan ve aralarindaki uzakligi bulup yazan program. (GitHub icin aciklama yapiyorum.) 
+
+#include <stdio.h>
+#include <math.h>
